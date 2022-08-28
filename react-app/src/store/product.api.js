0e645c950c48b7aa94446683a50dbc94514dd6bc
zen37 <zen37>
2022-08-28 11:29:39 -0700

    add products

--- react-app/src/store/product.api.js
+++ react-app/src/store/product.api.js
@@ -18,6 +18,18 @@ const data = {
       description: 'Bag of 7 fresh McIntosh apples',
       quantity: 1,
     },
+    {
+      id: 40,
+      name: 'Ice Cream',
+      description: 'Chocolate and peanut ice cream',
+      quantity: 1,
+    },
+    {
+      id: 50,
+      name: 'Potatoes',
+      description: 'Bag of 5lb',
+      quantity: 1,
+    },   
   ],
 };
 
